479e1590f6b66d7ad0e2ca87106daa8fce5bf2dc
Dane Burchette <dane.burchette@gmail.com>
2025-02-11 21:23:09 -0500

    Precompiled Binary Update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
+echo "Building Linux Binary"
 go build -o bin/Randomizer src/main.go
 export GOOS=windows
 export GOARCH=amd64
+echo "Building Windows Binary"
 go build -o bin/Randomizer.exe src/main.go
